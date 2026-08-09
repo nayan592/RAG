4cd6854282426d519b71e25b1e612f92528bcc9d
nayanchudasama <naync59@gmail.com>
2026-08-09 14:36:24 +0530

    Initial commit

---
+++ ingetion_pipline.py
@@ -0,0 +1,124 @@
+import os
+from langchain_community.document_loaders import TextLoader, DirectoryLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def load_documents(docs_path="docs"):
+    """Load all text files from the docs directory"""
+    print(f"Loading documents from {docs_path}...")
+    
+    # Check if docs directory exists
+    if not os.path.exists(docs_path):
+        raise FileNotFoundError(f"The directory {docs_path} does not exist. Please create it and add your company files.")
+    
+    # Load all .txt files from the docs directory
+    loader = DirectoryLoader(
+        path=docs_path,
+        glob="*.txt",
+        loader_cls=TextLoader
+    )
+    
+    documents = loader.load()
+    
+    if len(documents) == 0:
+        raise FileNotFoundError(f"No .txt files found in {docs_path}. Please add your company documents.")
+    
+   
+    # for i, doc in enumerate(documents):  # Show first 2 documents
+    #     print(f"\nDocument {i+1}:")
+    #     print(f"  Source: {doc.metadata['source']}")
+    #     print(f"  Content length: {len(doc.page_content)} characters")
+    #     print(f"  Content preview: {doc.page_content[:100]}...")
+    #     print(f"  metadata: {doc.metadata}")
+
+    return documents
+
+def split_documents(documents, chunk_size=1000, chunk_overlap=0):
+    """Split documents into smaller chunks with overlap"""
+    print("Splitting documents into chunks...")
+    
+    text_splitter = CharacterTextSplitter(
+        chunk_size=chunk_size, 
+        chunk_overlap=chunk_overlap
+    )
+    
+    chunks = text_splitter.split_documents(documents)
+    
+    # if chunks:
+    
+    #     for i, chunk in enumerate(chunks[:5]):
+    #         print(f"\n--- Chunk {i+1} ---")
+    #         print(f"Source: {chunk.metadata['source']}")
+    #         print(f"Length: {len(chunk.page_content)} characters")
+    #         print(f"Content:")
+    #         print(chunk.page_content)
+    #         print("-" * 50)
+        
+    #     if len(chunks) > 5:
+    #         print(f"\n... and {len(chunks) - 5} more chunks")
+    
+    return chunks
+
+
+def create_vector_store(chunks, persist_directory="db/chroma_db"):
+    """Create and persist ChromaDB vector store."""
+
+    print("Creating embeddings and storing in ChromaDB...")
+
+    embedding_model = OpenAIEmbeddings(
+        model="openai/text-embedding-3-small",
+        api_key=os.getenv("OPENROUTER_API_KEY"),
+        base_url="https://openrouter.ai/api/v1"
+    )
+
+    print("--- Creating vector store ---")
+
+    vectorstore = Chroma.from_documents(
+        documents=chunks,
+        embedding=embedding_model,
+        persist_directory=persist_directory,
+        collection_metadata={"hnsw:space": "cosine"}
+    )
+
+    print("--- Finished creating vector store ---")
+
+    return vectorstore
+
+def main():
+    print("Main Function")
+
+    persistent_directory = "db/chroma_db"
+    
+    # Check if vector store already exists
+    if os.path.exists(persistent_directory):
+        print("✅ Vector store already exists. No need to re-process documents.")
+        
+        embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+        vectorstore = Chroma(
+            persist_directory=persistent_directory,
+            embedding_function=embedding_model, 
+            collection_metadata={"hnsw:space": "cosine"}
+        )
+        print(f"Loaded existing vector store with {vectorstore._collection.count()} documents")
+        return vectorstore
+    
+    print("Persistent directory does not exist. Initializing vector store...\n")
+    # Load documents from the docs directory
+    documents = load_documents(docs_path="docs")
+    print(f"Total documents loaded: {len(documents)}")
+
+   # Split documents into chunks
+    chunks= split_documents(documents)
+    print(f"Total chunks created: {len(chunks)}")
+
+    # Create and persist ChromaDB vector store
+    vectorstore = create_vector_store(chunks)
+    
+
+
+if __name__ == "__main__":
+    main()
